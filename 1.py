addb9fa1b3630be13161234e034d418418562754
MariiaSam <kontragent@ukr.net>
2024-06-20 15:32:44 +0300

    fix 1.py

--- 1.py
+++ 1.py
@@ -35,7 +35,7 @@ from datetime import datetime
 def get_days_from_today(date):
     try:
         date_object = datetime.strptime(date, '%Y-%m-%d')
-        current_date = datetime.date.today()
+        current_date = datetime.today()
         difference_between_days = current_date - date_object
         return difference_between_days.days
     except:
