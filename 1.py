38e301f1fd4985e345a123b2d14147f2cf37a21e
MariiaSam <kontragent@ukr.net>
2024-06-20 15:32:24 +0300

    tasks 1

--- 1.py
+++ 1.py
@@ -29,3 +29,17 @@
 Читабельність коду: код повинен бути чистим і добре документованим.
 
 '''
+from datetime import datetime
+
+
+def get_days_from_today(date):
+    try:
+        date_object = datetime.strptime(date, '%Y-%m-%d')
+        current_date = datetime.date.today()
+        difference_between_days = current_date - date_object
+        return difference_between_days.days
+    except:
+        return 'Invalid format'
+
+
+print(get_days_from_today('2020-10-09'))
